30ed514ae29b028e661491e3f4c657a7c6d12bd8
4Bee <4bee.code@gmail.com>
2024-07-22 04:03:53 +0900

    🚩: module.scss 형식을 적용

--- src/Page/Main/components/Main.tsx
+++ src/Page/Main/components/Main.tsx
@@ -1,8 +1,9 @@
 import { test } from "../functions/function";
+import styles from "../scss/main.module.scss";
 
 const Main = () => {
   return (
-    <div>
+    <div className={styles.test}>
       <div>{test()}</div>
       <div>Main.tsx</div>
     </div>
--- src/Page/Scapture/components/Scapture.tsx
+++ src/Page/Scapture/components/Scapture.tsx
@@ -1,8 +1,10 @@
 import { test } from "../functions/function";
 import "../scss/scapture.scss";
+import styles from "../scss/scapture.module.scss";
+
 const Scapture = () => {
   return (
-    <div className="test">
+    <div className={styles.test}>
       <div>{test()}</div>
       <div>Scapture.tsx</div>
     </div>
