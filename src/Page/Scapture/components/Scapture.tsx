dacc5cfc751cfb08501084f75b7ab6d57b8847fe
4Bee <109135643+4BFC@users.noreply.github.com>
2024-07-21 17:09:53 +0900

    개발 협업을 위한 디렉토리 구조 적용 (#2)
    * 📝: 깃허브 문서화를 위해 issue,pr 양식 제작
    
    * 🚩: 각 페이지에 맞는 디렉토리 생성

---
+++ src/Page/Scapture/components/Scapture.tsx
@@ -0,0 +1,12 @@
+import { test } from "../functions/function";
+
+const Scapture = () => {
+  return (
+    <div>
+      <div>{test()}</div>
+      <div>Main.tsx</div>
+    </div>
+  );
+};
+
+export default Scapture;
---
+++ src/Page/Video/components/Video.tsx
@@ -0,0 +1,12 @@
+import { test } from "../functions/function";
+
+const Video = () => {
+  return (
+    <div>
+      <div>{test()}</div>
+      <div>Main.tsx</div>
+    </div>
+  );
+};
+
+export default Video;
