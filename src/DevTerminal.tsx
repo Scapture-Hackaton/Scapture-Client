e5af3d7979c4f1c3809af1134fed1750b18e3ccf
4Bee <4bee.code@gmail.com>
2024-07-23 00:57:53 +0900

    💄: DevTerminal 페이지에 Header와 Footer 적용 및 점검

--- src/DevTerminal.tsx
+++ src/DevTerminal.tsx
@@ -1,57 +1,64 @@
+import Header from './Page/Header/components/Header';
+import Footer from './Page/Footer/components/Footer';
+
 import { Link } from 'react-router-dom';
 import styles from './dev-ter.module.scss';
 
 const DevTerminal = () => {
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navList}>
-        <li>
-          <a href="">
-            <Link to="/">Main</Link>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Link to="/scapture">Scapture</Link>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Link to="/stadium">Stadium</Link>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Link to="/reservation">Reservation</Link>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Link to="/video">Video</Link>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Link to="/mypage">My Page</Link>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Link to="/header">Header</Link>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Link to="/footer">Footer</Link>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <Link to="/denav-ter">Dev Terminal</Link>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.navbar}>
+      <Header />
+      <nav className={styles.navbar}>
+        <ul className={styles.navList}>
+          <li>
+            <a href="">
+              <Link to="/">Main</Link>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Link to="/scapture">Scapture</Link>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Link to="/stadium">Stadium</Link>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Link to="/reservation">Reservation</Link>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Link to="/video">Video</Link>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Link to="/mypage">My Page</Link>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Link to="/header">Header</Link>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Link to="/footer">Footer</Link>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <Link to="/denav-ter">Dev Terminal</Link>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <Footer />
+    </div>
   );
 };
 
