e56c2089597c81884729ba14160c2f35c166fd3d
4Bee <4bee.code@gmail.com>
2024-07-22 22:39:45 +0900

    🚩: dev-terminal scss 수정

--- src/DevTerminal.tsx
+++ src/DevTerminal.tsx
@@ -1,51 +1,53 @@
 import { Link } from 'react-router-dom';
-import './dev-ter.module.scss';
+import styles from './dev-ter.module.scss';
 
 const DevTerminal = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/">Main</Link>
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li>
+          <a href="">
+            <Link to="/">Main</Link>
+          </a>
         </li>
-        <li className="nav-item">
+        <li>
           <a href="">
             <Link to="/scapture">Scapture</Link>
           </a>
         </li>
-        <li className="nav-item">
+        <li>
           <a href="">
             <Link to="/stadium">Stadium</Link>
           </a>
         </li>
-        <li className="nav-item">
+        <li>
           <a href="">
             <Link to="/reservation">Reservation</Link>
           </a>
         </li>
-        <li className="nav-item">
+        <li>
           <a href="">
             <Link to="/video">Video</Link>
           </a>
         </li>
-        <li className="nav-item">
+        <li>
           <a href="">
             <Link to="/mypage">My Page</Link>
           </a>
         </li>
-        <li className="nav-item">
+        <li>
           <a href="">
             <Link to="/header">Header</Link>
           </a>
         </li>
-        <li className="nav-item">
+        <li>
           <a href="">
             <Link to="/footer">Footer</Link>
           </a>
         </li>
-        <li className="nav-item">
+        <li>
           <a href="">
-            <Link to="/dev-ter">Dev Terminal</Link>
+            <Link to="/denav-ter">Dev Terminal</Link>
           </a>
         </li>
       </ul>
