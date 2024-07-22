63a51aa1090636b29e238fa8a03213c629e906ee
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-07-22 13:10:25 +0900

    ✅ ESLint 설정과 Prettier 설정을 환경에 맞게 설정한다.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,73 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'prettier',
+    'react-hooks',
+    'prefer-arrow',
+    'import',
+    'react',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+      },
+    ],
+    camelcase: ['error', { properties: 'always' }],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'react/button-has-type': 'off',
+    'jsx-a11y/anchor-has-content': 'off',
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
+      },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'no-console': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        ignoreRestSiblings: false,
+        varsIgnorePattern: '^(is|set)[A-Z].*$',
+      },
+    ],
   },
-}
+};
