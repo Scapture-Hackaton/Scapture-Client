395ddb734d4b7b69a2923139872e389b022b349e
4Bee <4bee.code@gmail.com>
2024-07-23 00:59:11 +0900

    💄: 각 페이지에 Header와 Footer 적용 및 불필요한 파일 삭제

--- src/Page/Main/components/Main.tsx
+++ src/Page/Main/components/Main.tsx
@@ -1,11 +1,16 @@
+import Header from '../../Header/components/Header';
+import Footer from '../../Footer/components/Footer';
+
 import { test } from '../functions/function';
 import styles from '../scss/main.module.scss';
 
 const Main = () => {
   return (
     <div className={styles.test}>
+      <Header />
       <div>{test()}</div>
       <div>Main.tsx</div>
+      <Footer />
     </div>
   );
 };
--- src/Page/Scapture/components/Scapture.tsx
+++ src/Page/Scapture/components/Scapture.tsx
@@ -1,12 +1,16 @@
+import Header from '../../Header/components/Header';
+import Footer from '../../Footer/components/Footer';
+
 import { test } from '../functions/function';
-import '../scss/scapture.scss';
 import styles from '../scss/scapture.module.scss';
 
 const Scapture = () => {
   return (
     <div className={styles.test}>
+      <Header />
       <div>{test()}</div>
       <div>Scapture.tsx</div>
+      <Footer />
     </div>
   );
 };
