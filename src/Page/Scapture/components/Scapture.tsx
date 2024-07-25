cafdd4d64f2578cb22ad2d1539d50f0cb2bbd5cb
4Bee <4bee.code@gmail.com>
2024-07-25 11:47:02 +0900

    🚩: Scapture 페이지 레이아웃 제작 세분화 필요 및 css 적용 필요

--- src/Page/Scapture/components/Scapture.tsx
+++ src/Page/Scapture/components/Scapture.tsx
@@ -1,15 +1,41 @@
 import Header from '../../Header/components/Header';
 import Footer from '../../Footer/components/Footer';
 
-import { test } from '../functions/function';
 import styles from '../scss/scapture.module.scss';
 
 const Scapture = () => {
   return (
     <div className={styles.test}>
       <Header />
-      <div>{test()}</div>
-      <div>Scapture.tsx</div>
+      <div className={styles.scapture}>
+        <div className={styles.banner}>
+          <img src="" alt="" />
+          <span>구장을 클릭하면,</span>
+          <span>새로운 플레이 그라운드가 열립니다.</span>
+        </div>
+        <div className={styles.option}>
+          <div id={styles.city}></div>
+          <div id={styles.local}></div>
+          <div id={styles.search}></div>
+        </div>
+        <div className={styles.stadiumList}>
+          <div className={styles.stadium}>
+            <div className={styles.stadiumImage}>
+              <img src="" alt="" />
+            </div>
+            <div className={styles.stadiumInfo}>
+              <div className={styles.stadium}>
+                <span id={styles.title}>구장 위치</span>
+                <span id={styles.info}>
+                  서울특별시 중구 장충동2가 산14-103번지
+                </span>
+                {/* css 적용 후 추가 예정 */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>Scapture.tsx</div>
+      </div>
       <Footer />
     </div>
   );
