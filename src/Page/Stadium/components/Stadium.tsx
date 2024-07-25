48232e6103775fa41acc283b3e8637cee7c1ed11
4Bee <4bee.code@gmail.com>
2024-07-25 19:51:18 +0900

    🚩: stadium 페이지 info 영역 css 수정 및 추가

--- src/Page/Stadium/components/Stadium.tsx
+++ src/Page/Stadium/components/Stadium.tsx
@@ -12,50 +12,55 @@ const Stadium = () => {
     <div className={styles.test}>
       <Header />
       <div className={styles.stadium}>
-        <div className={styles.banner}>
-          <img src={stadiumInfoImage} alt="" />
+        <div>
+          <div className={styles.banner}>
+            <img src={stadiumInfoImage} alt="" />
+          </div>
+          <div className={styles.intro}>
+            <div className={styles.title}>
+              <img src={elementImage} alt="" />
+              <span>창골축구장 (FC서울 축구교실)</span>
+            </div>
+            <div className={styles.introText}>
+              <span>
+                간단한 소개글 들어갈 영역 입니다. 해당 구장은 00소재 인조잔디로
+                평탄화 정도가 좋으며 우천시에도 미끄럽지 않고 여름날씨에도
+                화상걱정 없이 즐겨 볼 수 있는 잔디입니다.
+              </span>
+            </div>
+          </div>
+          <div className={styles.info}>
+            <div id={styles.stadium}>구장정보</div>
+            <div className={styles.group}>
+              <span className={styles.title} id={styles.location}>
+                구장 위치
+              </span>
+              <span id={styles.info}>
+                서울특별시 중구 장충동2가 산14-103번지
+              </span>
+            </div>
+            <div>
+              <span className={styles.title} id={styles.location}>
+                운영 시간
+              </span>
+              <span id={styles.info}>7:00 ~ 19:00</span>
+            </div>
+            <div>
+              <span className={styles.title} id={styles.location}>
+                실내/실외
+              </span>
+              <span id={styles.info}>실외</span>
+            </div>
+            <div>
+              <span className={styles.title} id={styles.location}>
+                주차 공간
+              </span>
+              <span id={styles.info}>주차 가능(66면) 5분당 150원</span>
+            </div>
+            <div id={styles.reservation}>구장 예약</div>
+          </div>
         </div>
-        <div className={styles.intro}>
-          <div className={styles.title}>
-            <img src={elementImage} alt="" />
-            <span>장골축구장 (FC서울 축구교실)</span>
-          </div>
-          <div className={styles.introText}>
-            <span>
-              간단한 소개글 들어갈 영역 입니다. 해당 구장은 00소재 인조잔디로
-              평탄화 정도가 좋으며 우천시에도 미끄럽지 않고 여름날씨에도
-              화상걱정 없이 즐겨 볼 수 있는 잔디입니다.
-            </span>
-          </div>
-        </div>
-        <div className={styles.info}>
-          <div className={styles.infoStadium}>구장정보</div>
-          <div className={styles.group}>
-            <span className={styles.title} id={styles.location}>
-              구장 위치
-            </span>
-            <span id={styles.info}>서울특별시 중구 장충동2가 산14-103번지</span>
-          </div>
-          <div>
-            <span className={styles.title} id={styles.location}>
-              운영 시간
-            </span>
-            <span id={styles.info}>7:00 ~ 19:00</span>
-          </div>
-          <div>
-            <span className={styles.title} id={styles.location}>
-              실내/실외
-            </span>
-            <span id={styles.info}>실외</span>
-          </div>
-          <div>
-            <span className={styles.title} id={styles.location}>
-              주차 공간
-            </span>
-            <span id={styles.info}>주차 가능(66면) 5분당 150원</span>
-          </div>
-          <div>구장 예약</div>
-        </div>
+
         <div className={styles.video}>
           <div className={styles.group}>
             <span>Today</span>
