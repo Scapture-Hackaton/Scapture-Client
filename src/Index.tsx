c5f7c9310b60f46682f8e2f282ad29579a402e8b
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-07-26 13:41:58 +0900

    💄 Root CSS 변경 (#19)

--- src/Index.tsx
+++ src/Index.tsx
@@ -5,8 +5,6 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="guide-line">
-      <App />
-    </div>
+    <App />
   </React.StrictMode>,
 );
