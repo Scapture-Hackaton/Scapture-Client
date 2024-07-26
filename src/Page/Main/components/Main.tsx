83aa75ca54494dbb68e6c0abdff5f040c3e996a8
4Bee <4bee.code@gmail.com>
2024-07-26 16:00:07 +0900

    🚩: main 페이지 초기 값을 기준으로 세팅

--- src/Page/Main/components/Main.tsx
+++ src/Page/Main/components/Main.tsx
@@ -16,7 +16,7 @@ const Main = () => {
     <div>
       <Header />
       <div className={styles.main}>
-        <div className={banner.banner}>
+        {/* <div className={banner.banner}>
           <div className={banner.object}>
             <div id={banner.right}></div>
             <div id={banner.blank}>
@@ -24,7 +24,7 @@ const Main = () => {
             </div>
             <div id={banner.left}></div>
           </div>
-        </div>
+        </div> */}
         <div className={styles.container}>
           <div className={styles.intro}>
             <div className={styles.introText}>
