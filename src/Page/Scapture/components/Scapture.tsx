94a92ec62d1a3423a5ba259f5becec4d1f1285a6
4Bee <109135643+4BFC@users.noreply.github.com>
2024-07-28 19:55:33 +0900

    🔀: Scapture 페이지의 구체적 css를 적용 및 검토 (#31)
    * 💄: 이미지 변경으로 인한 이미지 제거 및 생성
    
    * 💄: footer 영역 css의 width 값과 margin값 적용
    
    * 💄: 이미지 변경으로 인한 이미지 제거 및 생성
    
    * 💄: header 영역 css의 width 값과 margin 값 적용
    
    * 🚩: footer,header 점검 및 수정
    
    * 🚩:  margin 값 보다 width 값으로 처리하는 것이 더 나은 방법
    
    * 💄: stadium 페이지 css의 width 값과 margin 값 적용
    
    * 💄: stadium페이지 레이아웃 조정 및 수정
    
    * 💄: stadium 페이지의 select 추가 및 css 적용 videoList영역 margin 확인 필요
    
    * 💄: stadium 페이지 css의 width 값과 margin 값 적용
    
    * 💄: scapture 페이지 css의 width 값과 margin 값 적용
    
    * 💄: input placeholder 추가

--- src/Page/Scapture/components/Scapture.tsx
+++ src/Page/Scapture/components/Scapture.tsx
@@ -19,18 +19,60 @@ const Scapture = () => {
           </div>
         </div>
         <div className={styles.option}>
-          <div className={styles.select}>
-            <select id={styles.city}>
-              <option value="">도시</option>
-            </select>
-            <select id={styles.local}>
-              <option value="">지역</option>
-            </select>
+          <div className={styles.container}>
+            <div className={styles.select}>
+              <select id={styles.city}>
+                <option value="">도시</option>
+              </select>
+              <select id={styles.local}>
+                <option value="">지역</option>
+              </select>
+            </div>
+            <div id={styles.search}>
+              <input type="text" placeholder="구장명 검색" />
+              <div id={styles.searchImage}>
+                <img src={searchImage} alt="" />
+              </div>
+            </div>
           </div>
-          <div id={styles.search}>
-            <input type="text" />
-            <div id={styles.searchImage}>
-              <img src={searchImage} alt="" />
+        </div>
+        <div className={styles.stadiumList}>
+          <div className={styles.stadium}>
+            <div className={styles.stadiumImage}>
+              <img src={stadiumImage} alt="" />
+            </div>
+            <div className={styles.stadiumInfo}>
+              <div className={styles.stadium}>
+                <span id={styles.name}>장충테니스장</span>
+                <div className={styles.info}>
+                  <div>
+                    <span className={styles.title} id={styles.location}>
+                      구장 위치
+                    </span>
+                    <span id={styles.info}>
+                      서울특별시 중구 장충동2가 산14-103번지
+                    </span>
+                  </div>
+                  <div>
+                    <span className={styles.title} id={styles.location}>
+                      운영 시간
+                    </span>
+                    <span id={styles.info}>7:00 ~ 19:00</span>
+                  </div>
+                  <div>
+                    <span className={styles.title} id={styles.location}>
+                      실내/실외
+                    </span>
+                    <span id={styles.info}>실외</span>
+                  </div>
+                  <div>
+                    <span className={styles.title} id={styles.location}>
+                      주차 공간
+                    </span>
+                    <span id={styles.info}>주차 가능(66면) 5분당 150원</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
