24a667f89c2d0563f3bede83c4accfe86eac7783
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-07-31 00:17:21 +0900

    🚩 도시, 지역에 따른 경기장 데이터 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
+
 import Main from './Page/Main/components/Main';
 import Scapture from './Page/Scapture/components/Scapture';
 import Stadium from './Page/Stadium/components/Stadium';
@@ -14,24 +20,28 @@ import DevTer from './DevTerminal';
 import './App.scss';
 import Community from './Page/Community/components/Community';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/scapture" element={<Scapture />} />
-        <Route path="/stadium" element={<Stadium />} />
-        <Route path="/reservation" element={<Reservation />} />
-        <Route path="/video" element={<Video />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/mypage/edit" element={<EditProfile />} />
-        <Route path="/mypage/reservation" element={<MyReservation />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/header" element={<Header />} />
-        <Route path="/footer" element={<Footer />} />
-        <Route path="/dev-ter" element={<DevTer />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/scapture" element={<Scapture />} />
+          <Route path="/stadium" element={<Stadium />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/video" element={<Video />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/edit" element={<EditProfile />} />
+          <Route path="/mypage/reservation" element={<MyReservation />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/header" element={<Header />} />
+          <Route path="/footer" element={<Footer />} />
+          <Route path="/dev-ter" element={<DevTer />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
--- src/Page/Scapture/components/Scapture.tsx
+++ src/Page/Scapture/components/Scapture.tsx
@@ -5,8 +5,98 @@ import bannerImage from '../image/banner-image.png';
 import stadiumImage from '../image/stadium-image.png';
 import searchImage from '../image/search-image.png';
 import styles from '../scss/scapture.module.scss';
+import SelectBtn from './SelectBtn';
+import { useState } from 'react';
+import { getStadiumList } from '../../../apis/api/scapture.api';
+import { useQuery } from '@tanstack/react-query';
+import { Stadium } from '../../../apis/dto/scapture.dto';
 
 const Scapture = () => {
+  const selectCity = ['서울시', '인천', '경기도'];
+  const selectState: Record<string, string[]> = {
+    서울시: [
+      '성북구',
+      '강서구',
+      '양천구',
+      '구로구',
+      '영등포구',
+      '관악구',
+      '금천구',
+      '동작구',
+      '서초구',
+      '강남구',
+      '송파구',
+      '강동구',
+      '광진구',
+      '중랑구',
+      '노원구',
+      '도봉구',
+      '강북구',
+      '동대문구',
+      '성동구',
+      '중구',
+      '용산군',
+      '종로구',
+      '서대문구',
+      '마포구',
+      '은평구',
+    ],
+
+    인천: [
+      '중구',
+      '동구',
+      '미추홀구',
+      '연수구',
+      '남동구',
+      '부평구',
+      '계양구',
+      '서구',
+      '강화군',
+      '웅진군',
+    ],
+
+    경기도: [
+      '김포',
+      '파주',
+      '고양',
+      '양주',
+      '의정부',
+      '남양주',
+      '하남',
+      '광주',
+      '성남',
+      '수원',
+      '과천',
+      '안양',
+      '광명',
+      '부천',
+      '시흥',
+      '안산',
+      '용인',
+    ],
+  };
+
+  // 선택된 도시에 따라 리스트 변경
+  const [isCity, setCity] = useState(selectCity[0]);
+  const [isState, setState] = useState(selectState[isCity][0]);
+
+  const handleCityChange = (city: string) => {
+    setCity(city);
+    setState(selectState[city][0]); // City 변경 시 state 리스트 초기화
+  };
+
+  const handleStateChange = (state: string) => {
+    setState(state);
+  };
+
+  const { data: stadiums } = useQuery({
+    queryKey: ['stadiums', isCity, isState],
+    queryFn: () => getStadiumList(isCity, isState),
+    initialData: [] as Stadium[],
+  });
+
+  console.log(stadiums);
+
   return (
     <div className={styles.test}>
       <Header />
@@ -21,12 +111,22 @@ const Scapture = () => {
         <div className={styles.option}>
           <div className={styles.container}>
             <div className={styles.select}>
-              <select id={styles.city}>
+              <SelectBtn
+                selectList={selectCity}
+                selectedOption={isCity}
+                onOptionChange={handleCityChange}
+              ></SelectBtn>
+              <SelectBtn
+                selectList={selectState[isCity]}
+                selectedOption={isState}
+                onOptionChange={handleStateChange}
+              ></SelectBtn>
+              {/* <select id={styles.city}>
                 <option value="">도시</option>
               </select>
               <select id={styles.local}>
                 <option value="">지역</option>
-              </select>
+              </select> */}
             </div>
             <div id={styles.search}>
               <input type="text" placeholder="구장명 검색" />
