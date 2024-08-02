9674d4bc73ad6ee32939ea8ad8c75a5374680105
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-08-02 11:44:51 +0900

    🚩 북마크 수정

--- src/apis/api/reservation.api.tsx
+++ src/apis/api/reservation.api.tsx
@@ -21,6 +21,26 @@ export const reserveField = async (scheduleId: number) => {
 };
 
 // 구장 예약 일정 조회
+// export const getReservationList = async (
+//   stadiumId: number,
+//   fieldId: number,
+//   date: string,
+// ) => {
+//   try {
+//     const res: CommonResponse = await authInstance.get(
+//       `api/reservations/${stadiumId}/${fieldId}?date=${date}`,
+//     );
+
+//     return res.data.data;
+//   } catch (e: any) {
+//     console.log(e);
+//     return {
+//       status: e.response.status,
+//     };
+//   }
+// };
+
+// 구장 예약 일정 조회
 export const getReservationList = async (stadiumId: number, date: string) => {
   try {
     const res: CommonResponse = await authInstance.get(
