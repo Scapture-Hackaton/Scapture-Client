32cfda64375152c408c14e1d41d564b668aa2751
4Bee <4bee.code@gmail.com>
2024-08-02 17:56:39 +0900

    🚩: atom interface 분리 및 확인

--- src/Page/MyPage/Atom/atom.tsx
+++ src/Page/MyPage/Atom/atom.tsx
@@ -9,14 +9,27 @@ interface userData {
   team: string | null;
 }
 
+interface bananaData {
+  balance: number;
+  subscribed: boolean;
+}
+
 export const userData = atom<userData>({
   key: 'userData',
   default: {
     endDate: null,
-    image: '',
+    image: 'undefined',
     location: null,
-    name: '',
-    role: '',
+    name: 'undefined',
+    role: 'undefined',
     team: null,
+  },
+});
+
+export const bananaData = atom<bananaData>({
+  key: 'bananaData',
+  default: {
+    balance: 0,
+    subscribed: false,
   },
 });
---
+++ src/Page/MyPage/MyPage/dto/atom.interface.tsx
@@ -0,0 +1,13 @@
+export interface userData {
+  endDate: string | null;
+  image: string | null;
+  location: string | null;
+  name: string;
+  role: string;
+  team: string | null;
+}
+
+export interface bananaData {
+  balance: number;
+  subscribed: boolean;
+}
