2062bcf2d05a99659382ab335b16cab459deb276
4Bee <4bee.code@gmail.com>
2024-08-04 01:25:58 +0900

    🚩: login시 login의 상태값을 저장하는 atom 생성

--- src/Page/Header/Atom/atom.tsx
+++ src/Page/Header/Atom/atom.tsx
@@ -1,7 +1,18 @@
 import { atom } from 'recoil';
 
-export const LoginResponse = atom<string>({
-  // 전역적으로 고유해야함
-  key: 'LoginResponse',
-  default: '',
+// export const LoginResponse = atom<string>({
+//   // 전역적으로 고유해야함
+//   key: 'LoginResponse',
+//   default: '',
+// });
+
+export interface loginData {
+  state: boolean;
+}
+
+export const loginDataAtom = atom<loginData>({
+  key: 'loginData',
+  default: {
+    state: false,
+  },
 });
