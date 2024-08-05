03ad78c8173e982b05075269691563570077cff6
4Bee <4bee.code@gmail.com>
2024-08-05 18:20:04 +0900

    🚩: video 페이지 BookMark, 좋아요 클릭 시 login 모달 활성화

--- src/Page/Video/components/Video.tsx
+++ src/Page/Video/components/Video.tsx
@@ -260,6 +260,7 @@ const Video = () => {
   const handleToggleLike = (isLiked: boolean) => {
     if (videoDetail && !isLiked) {
       toggleLike(videoId);
+      modalNotice(loginModalRef);
     } else if (videoDetail && isLiked) {
       toggleUnLike(videoId);
     }
@@ -300,6 +301,7 @@ const Video = () => {
   const handleToggleStore = (isStore: boolean) => {
     if (videoDetail && !isStore) {
       toggleStore(videoId);
+      modalNotice(loginModalRef);
     } else if (videoDetail && isStore) {
       toggleUnStore(videoId);
     }
