b7572ddeb4a875bc869a8456a79d2d6281fccd87
4Bee <4bee.code@gmail.com>
2024-08-05 18:28:35 +0900

    🚩: reservation 페이지에서 예약버튼 클릭시 login 모달 활성화

--- src/Page/Reservation/components/ReservationList.tsx
+++ src/Page/Reservation/components/ReservationList.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from '../scss/reservation.module.scss';
+import loginModal from '../../Header/scss/login-modal.module.scss';
 import { ReservationDto } from '../../../apis/dto/reservation.dto';
+import { LoginModal } from '../../Header/components/LoginModal';
+import {
+  GOOGLE_AUTH_URL,
+  KAKAO_AUTH_URL,
+  NAVER_AUTH_URL,
+} from '../../../apis/config/login.config';
+import { modalNotice } from '../functions/ModalFunction';
 
 interface ReservationListProps {
   reserveList: ReservationDto[][];
@@ -10,6 +18,15 @@ const ReservationList: React.FC<ReservationListProps> = ({ reserveList }) => {
   const reserve = (scheduleId: number) => {
     console.log(scheduleId);
   };
+
+  const loginModalRef = useRef<HTMLDialogElement>(null);
+  //Object
+  const AUTH_URLS = {
+    kakao: KAKAO_AUTH_URL,
+    google: GOOGLE_AUTH_URL,
+    naver: NAVER_AUTH_URL,
+  };
+
   return (
     <>
       {Array.isArray(reserveList) && reserveList.length > 0 ? (
@@ -28,6 +45,7 @@ const ReservationList: React.FC<ReservationListProps> = ({ reserveList }) => {
                   <button
                     onClick={() => {
                       reserve(item.scheduleId);
+                      modalNotice(loginModalRef);
                     }}
                   >
                     예약하기
@@ -40,6 +58,11 @@ const ReservationList: React.FC<ReservationListProps> = ({ reserveList }) => {
       ) : (
         <div>No reservations available</div>
       )}
+      <LoginModal
+        styles={loginModal}
+        AUTH_URLS={AUTH_URLS}
+        modalRef={loginModalRef}
+      ></LoginModal>
     </>
   );
 };
