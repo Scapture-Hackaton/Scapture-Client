1c593404fd6fc1c98e74b4cb1ed18558862ab25d
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-08-22 14:49:09 +0900

    파일 다운로드 방지 (#130)
    * 🚩 파일 다운로드 방지 테스트
    
    * ✨ Video 다운로드 방지

--- src/Page/Video/components/Video.tsx
+++ src/Page/Video/components/Video.tsx
@@ -317,6 +317,48 @@ const Video = () => {
     }
   };
 
+  const [isBlobUrl, setBlobUrl] = useState('');
+
+  const loadVideo = async () => {
+    try {
+      if (isBlobUrl) {
+        // 기존 Blob URL을 해제하여 메모리 누수 방지
+        URL.revokeObjectURL(isBlobUrl);
+        setBlobUrl('');
+      }
+
+      // 비디오 데이터를 모두 다운로드
+      const response = await fetch(videoDetail.video);
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+
+      const videoElement = document.getElementById(
+        'videoPlayer',
+      ) as HTMLVideoElement;
+
+      if (videoElement) {
+        videoElement.src = url;
+
+        // 비디오가 끝까지 재생된 후 Blob URL 해제
+        videoElement.onended = () => {
+          URL.revokeObjectURL(url);
+          setBlobUrl(''); // 해제 후 상태 초기화
+        };
+      }
+
+      // 새로운 Blob URL 상태 설정
+      setBlobUrl(url);
+    } catch (error) {
+      console.error('비디오 로딩 중 오류가 발생했습니다.', error);
+    }
+  };
+
+  useEffect(() => {
+    if (videoDetail && videoDetail.video) {
+      loadVideo();
+    }
+  }, [videoDetail]);
+
   return (
     <div className={styles.test}>
       <Header />
@@ -325,9 +367,12 @@ const Video = () => {
           <>
             <div className={styles.videoContainer}>
               <div className={styles.video}>
-                <video controls>
-                  <source src={videoDetail.video}></source>
-                </video>
+                <video
+                  id="videoPlayer"
+                  controls
+                  controlsList="nodownload"
+                  onContextMenu={e => e.preventDefault()}
+                ></video>
               </div>
               <div className={styles.group}>
                 <div className={styles.title}>{videoDetail.name}</div>
