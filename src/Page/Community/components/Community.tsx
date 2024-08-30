724cc1a7cbfde805677e7910d23726a3cea2795d
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-08-30 20:13:47 +0900

    🚩 댓글창 레이아웃 구현

--- src/Page/Community/components/Community.tsx
+++ src/Page/Community/components/Community.tsx
@@ -3,8 +3,12 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import Header from '../../Header/components/Header';
 import Footer from '../../Footer/components/Footer';
 import styles from '../scss/community.module.scss';
-import modal from '../../Header/scss/login-modal.module.scss';
+import modal from '../../Video/scss/video-modal.module.scss';
+import loginModal from '../../Header/scss/login-modal.module.scss';
 import Comment from './Comment';
+
+import download from '../../../assets/Icon/downLoadIcon.svg';
+import share from '../../../assets/Icon/shareIcon.svg';
 
 import {
   KAKAO_AUTH_URL,
@@ -21,10 +25,27 @@ import PopularVideoList from './PopularVideoList';
 import { likesVideo } from '../../../apis/api/stadium.api';
 import VideoHeart from './VideoHeart';
 import { LoginModal } from '../../Header/components/LoginModal';
+import BookMark from './BookMark';
+import {
+  checkAuthDownloadVideo,
+  downloadVideo,
+  storeVideo,
+  unStoreVideo,
+} from '../../../apis/api/video.api';
+import { modalNotice } from '../functions/ModalFunction';
+import { VideoModal } from '../../Video/components/VideoModal';
+
+import locationImg from '../../../assets/Icon/location.svg';
+import clock from '../../../assets/Icon/Clock.svg';
+import parking from '../../../assets/Icon/parking.svg';
+
+import dropDown from '../../../assets/Icon/dropDown.svg';
+import upArrow from '../../../assets/Icon/upArrow.svg';
 
 const Community = () => {
   //DOM
   const modalRef = useRef<HTMLDialogElement>(null);
+  const loginModalRef = useRef<HTMLDialogElement>(null);
 
   //Object
   const AUTH_URLS = {
@@ -185,11 +206,112 @@ const Community = () => {
     }
   };
 
+  // useMutation 훅을 사용하여 영상 저장/해제 처리
+  const { mutate: toggleStore } = useMutation({
+    mutationFn: async (videoId: number) => {
+      const res = await storeVideo(videoId);
+      if (res.status == 400 || res.status == 403) {
+        modalNotice(modalRef);
+      }
+    },
+    onSuccess: () => {
+      // 비디오 상세 정보 갱신
+      queryClient.invalidateQueries({
+        queryKey: ['videoDetail', isVideoId],
+      });
+    },
+    onError: error => {
+      console.error('영상 저장 중 오류가 발생했습니다.', error);
+    },
+  });
+
+  // 다운로드 기능
+  const handleDownloadClick = async () => {
+    try {
+      const authResponse = await checkAuthDownloadVideo(isVideoId);
+
+      if (authResponse.status === 200 || authResponse.status === 409) {
+        const downloadResponse = await downloadVideo(isVideoId);
+
+        if (downloadResponse.status === 200) {
+          fetch(`${videoDetailData.video}`, {
+            method: 'GET',
+          })
+            .then(response => response.blob())
+            .then(blob => {
+              const url = window.URL.createObjectURL(blob);
+              const link = document.createElement('a');
+
+              link.setAttribute('href', url);
+              link.setAttribute('download', `${videoDetailData.name}.mp4`);
+
+              document.body.appendChild(link);
+
+              link.click();
+
+              link.parentNode?.removeChild(link);
+
+              window.URL.revokeObjectURL(url);
+            });
+        } else {
+          alert('로그인이 필요합니다.');
+          modalNotice(loginModalRef);
+        }
+      } else {
+        modalNotice(loginModalRef);
+      }
+    } catch (error) {
+      console.error('비디오 다운로드 중 오류가 발생했습니다.', error);
+    } finally {
+      modalRef.current?.close(); // 다운로드 완료 후 모달 닫기
+    }
+  };
+
+  const { mutate: toggleUnStore } = useMutation({
+    mutationFn: async (videoId: number) => {
+      const res = await unStoreVideo(videoId);
+      if (res.status == 400 || res.status == 403) {
+        modalNotice(modalRef);
+      }
+    },
+    onSuccess: () => {
+      // 비디오 상세 정보 갱신
+      queryClient.invalidateQueries({
+        queryKey: ['videoDetail', isVideoId],
+      });
+    },
+    onError: error => {
+      console.error('영상 저장 해제 중 오류가 발생했습니다.', error);
+    },
+  });
+
+  // 북마크를 눌렀을 때 처리
+  const handleToggleStore = (isStore: boolean) => {
+    if (videoDetailData && !isStore) {
+      toggleStore(isVideoId);
+    } else if (videoDetailData && isStore) {
+      toggleUnStore(isVideoId);
+    }
+  };
+
   useEffect(() => {
     if (videoDetailData && videoDetailData.video) {
       loadVideo();
     }
   }, [videoDetailData]);
+
+  const handelOpenDownloadModal = () => {
+    modalNotice(modalRef);
+  };
+
+  // 버튼을 눌렀는지에 대한 상태
+  const [open, setOpen] = useState(false);
+
+  const selectRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className={styles.test}>
@@ -199,29 +321,105 @@ const Community = () => {
           <>
             <div className={styles.videoContainer}>
               <div className={styles.video}>
-                {/* <img src={videoDetailData.image} alt="" /> */}
                 <video
-                  id="communityVideo"
+                  id="videoPlayer"
                   controls
                   controlsList="nodownload"
                   onContextMenu={e => e.preventDefault()}
-                >
-                  {/* <source src={videoDetailData.video} /> */}
-                </video>
+                ></video>
               </div>
-              <div className={styles.title}>{videoDetailData.name}</div>
+              <div className={styles.group}>
+                <div className={styles.title}>{videoDetailData.name}</div>
+                <ul className={styles.icons}>
+                  <li className={styles.heart}>
+                    <VideoHeart
+                      videoId={isVideoId}
+                      isLiked={videoDetailData.isLiked}
+                      likeCount={videoDetailData.likeCount}
+                      onToggleLike={handleToggleLike}
+                    />
+                  </li>
+                  <li>
+                    <BookMark
+                      stored={videoDetailData.isStored}
+                      onToggleStore={handleToggleStore}
+                    ></BookMark>
+                  </li>
+                  <li onClick={handelOpenDownloadModal}>
+                    <img src={download} alt="" width="20px" height="20px"></img>
+                    <p>다운로드</p>
+                  </li>
+                  <li>
+                    <img src={share} alt="" width="20px" height="20px"></img>
+                    <p>공유</p>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className={styles.fieldText}>
+            <div
+              className={`${styles.infoBox}  ${open ? styles.open : ''}`}
+              ref={selectRef}
+            >
+              <div className={styles.infoHeader}>
+                <div>구장 정보</div>
+                {open ? (
+                  <img
+                    src={upArrow}
+                    alt=""
+                    width="20px"
+                    height="20px"
+                    onClick={toggleDropdown}
+                  ></img>
+                ) : (
+                  <img
+                    src={dropDown}
+                    alt=""
+                    width="20px"
+                    height="20px"
+                    onClick={toggleDropdown}
+                  ></img>
+                )}
+              </div>
+
+              <div className={styles.infoGroup}>
+                <div className={styles.topInfo}>
+                  <div className={styles.isOutside}>
+                    {/* {stadiumDetail.isOutside ? '실외' : '실내'} */}
+                    실외
+                  </div>
+                  <div className={styles.isParking}>
+                    {/* {stadiumDetail.isOutside ? '주차 가능' : '주차 불가능'} */}
+                    주차 가능
+                  </div>
+                </div>
+
+                <div id={styles.info}>
+                  <div className={styles.line}>
+                    <img src={locationImg} alt="" width="20px" height="20px" />
+                    {/* <div className={styles.info}>{stadiumDetail.location}</div> */}
+                    <div className={styles.info}>주소</div>
+                  </div>
+
+                  <div className={styles.line}>
+                    <img src={clock} alt="" width="20px" height="20px" />
+                    {/* <span className={styles.info}>{stadiumDetail.hours}</span> */}
+                    <span className={styles.info}>00:00 ~ 00:00</span>
+                  </div>
+
+                  <div className={styles.line}>
+                    <img src={parking} alt="" width="20px" height="20px" />
+
+                    {/* <span className={styles.info}>{stadiumDetail.parking}</span> */}
+                    <span className={styles.info}>유료 주차</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className={styles.fieldText}>
               {videoDetailData.stadium.name}
-            </div>
-            <div className={styles.heart}>
-              <VideoHeart
-                videoId={isVideoId}
-                isLiked={videoDetailData.isLiked}
-                likeCount={videoDetailData.likeCount}
-                onToggleLike={handleToggleLike}
-              />
-            </div>
+            </div> */}
+
             <div className={styles.commentContainer}>
               <Comment videoId={isVideoId} />
             </div>
@@ -239,10 +437,16 @@ const Community = () => {
         </div> */}
       </div>
       <Footer />
-      <LoginModal
+
+      <VideoModal
         styles={modal}
+        ref={modalRef}
+        handleDownloadClick={handleDownloadClick}
+      />
+      <LoginModal
+        styles={loginModal}
         AUTH_URLS={AUTH_URLS}
-        modalRef={modalRef}
+        modalRef={loginModalRef}
       ></LoginModal>
     </div>
   );
