74eb3bf41e14ee88cb303c678b03ad0e44daa623
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-08-30 16:13:30 +0900

    Reservation 페이지 추가 레이아웃 제작 (#146)
    * 🚩 예약 리스트 css 변경
    
    * 🚩 Modal 레이아웃 작업
    
    * 🚩 테스트 코드 주석

--- src/Page/Reservation/components/ReservationList.tsx
+++ src/Page/Reservation/components/ReservationList.tsx
@@ -15,6 +15,8 @@ import { ReservationCheckModal, ReservationModal } from './ReservationModal';
 import modal from '../scss/reservation-modal.module.scss';
 import check from '../scss/reservation-check-modal.module.scss';
 
+import noDataIcon from '../../../assets/Icon/noDataIcon.svg';
+
 interface ReservationListProps {
   reserveList: ReservationDto[][];
   queryClient: any;
@@ -76,6 +78,10 @@ const ReservationList: React.FC<ReservationListProps> = ({
     naver: NAVER_AUTH_URL,
   };
 
+  const successReserve = () => {
+    modalNotice(modalCheckRef);
+  };
+
   const handleReserveConfirm = (scheduleId: number) => {
     mutation.mutate(scheduleId);
   };
@@ -107,8 +113,49 @@ const ReservationList: React.FC<ReservationListProps> = ({
           </div>
         ))
       ) : (
-        <div>No reservations available</div>
+        <div className={styles.noData}>
+          <img
+            src={noDataIcon}
+            alt="검색 결과가 없습니다."
+            width="180px"
+            height="180px"
+          />
+          <div>검색 결과가 없어요</div>
+        </div>
       )}
+
+      {/* <div className={styles.group}>
+        <div className={styles.compontent}>
+          <div className={styles.field}>A구장장장장장장</div>
+
+          <div className={styles.info}>
+            <div className={styles.date}>10:00 ~ 12:00</div>
+            <div className={styles.versus}>6 vs 6</div>
+
+            <button
+              onClick={() => {
+                modalNotice(modalRef);
+              }}
+            >
+              예약하기
+            </button>
+          </div>
+        </div>
+
+        <div className={styles.compontent}>
+          <div className={styles.field}>A구장</div>
+
+          <div className={styles.info}>
+            <div className={styles.date}>10:00 ~ 12:00</div>
+            <div className={styles.versus}>6 vs 6</div>
+
+            <button className={styles.booked} disabled>
+              예약하기
+            </button>
+          </div>
+        </div>
+      </div> */}
+
       <LoginModal
         styles={loginModal}
         AUTH_URLS={AUTH_URLS}
@@ -119,6 +166,7 @@ const ReservationList: React.FC<ReservationListProps> = ({
         styles={modal}
         ref={modalRef}
         reservation={selectedReservation}
+        onSuccess={successReserve}
         onConfirm={handleReserveConfirm}
       />
       {/* modalCheckRef */}
