6c3e208672275ffd12c4147868fbae07a4434cd4
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-10-11 18:42:29 +0900

    🚩 녹화 시간 리스트 추가

--- src/Page/MyPage/Admin/components/Camera/CameraControl.tsx
+++ src/Page/MyPage/Admin/components/Camera/CameraControl.tsx
@@ -16,7 +16,7 @@ interface CameraControlProps {
 
 const SOCKET_SERVER_IP = `${import.meta.env.VITE_SOCKET_SERVER_IP}`;
 
-const timeList = ['5', '10', '15', '20', '25', '30'];
+const timeList = ['1', '3', '5', '10', '15', '20', '25', '30'];
 
 const CameraControl: React.FC<CameraControlProps> = ({ fields }) => {
   // 페이지에 보여줄 시간
