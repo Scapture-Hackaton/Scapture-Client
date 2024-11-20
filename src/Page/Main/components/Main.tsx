d05aee3a820baf4abc6ceac71b36bdd00c05e16b
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-11-20 18:43:12 +0900

    🚩 Redirect 기능 구현

--- src/Page/Main/components/Main.tsx
+++ src/Page/Main/components/Main.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMainStadium } from '../../../apis/api/main.api';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useEffect, useRef } from 'react';
 
 import styles from '../scss/main.module.scss';
@@ -37,13 +37,13 @@ import stadiumEx from '../image/stadiumEx.jpeg';
 import testImg from '../../../assets/image/test.png';
 
 const Main = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const redirectPage = localStorage.getItem('Redirect');
 
-  const locationQuery = searchParams.get('location');
-
-  if (locationQuery) {
-    window.location.href = locationQuery;
+  if (redirectPage) {
+    window.location.href = redirectPage;
+    localStorage.removeItem('Redirect');
   }
+
   const { data: mainData, isSuccess: isMainDataSuccess } = useQuery({
     queryKey: ['main_stadium'],
     queryFn: () => getMainStadium(),
