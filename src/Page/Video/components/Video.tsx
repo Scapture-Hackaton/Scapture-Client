9753f32b390a9e3c5cdd2a5dae194253a33d6b78
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-12-13 13:35:19 +0900

    🚑️ Cloudfront url 변경

--- src/Page/Video/components/Video.tsx
+++ src/Page/Video/components/Video.tsx
@@ -308,6 +308,8 @@ const Video = () => {
     refetchVideoDetail(); // React Query로 새로운 videoId의 데이터 가져오기
   }, [videoId]); // currentVideoId가 변경될 때마다 실행
 
+  console.log(videoDetail);
+
   return (
     <div className={styles.test}>
       <Header index={2} />
@@ -331,7 +333,10 @@ const Video = () => {
                   id="videoPlayer"
                   controls
                   controlsList="nodownload"
-                  src={videoDetail.video}
+                  src={videoDetail.video.replace(
+                    'd1yr3xnm7vncof.cloudfront.net',
+                    'd1f9epsweambhy.cloudfront.net',
+                  )}
                   onContextMenu={e => e.preventDefault()}
                 ></video>
 
